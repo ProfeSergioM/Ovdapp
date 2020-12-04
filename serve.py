725396222455a7586de14cbb8fb81833f1c29f7d
ProfeSergioM <sergio.morales@sernageomin.cl>
2020-12-04 10:06:39 -0300

    removed example.log

--- serve.py
+++ serve.py
@@ -1,9 +1,6 @@
 from waitress import serve
 from index import app
 import logging
-logging.basicConfig( level=logging.DEBUG, filename='example.log')
-logger = logging.getLogger('waitress')
-logger.setLevel(logging.DEBUG)
 if __name__ == '__main__':
 
     serve(app.server, threads=100, port=8080,host='0.0.0.0')
--- serve8081.py
+++ serve8081.py
@@ -1,9 +1,6 @@
 from waitress import serve
 from index8081 import app
 import logging
-logging.basicConfig( level=logging.DEBUG, filename='example.log')
-logger = logging.getLogger('waitress')
-logger.setLevel(logging.DEBUG)
 if __name__ == '__main__':
 
     serve(app.server, threads=100, port=8081,host='0.0.0.0')
