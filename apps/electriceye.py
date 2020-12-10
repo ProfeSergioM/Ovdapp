9fb8d95477cff27dd19d9f66dd124f799243e858
ProfeSergioM <sergio.morales@sernageomin.cl>
2020-12-10 17:18:25 -0300

    Update electriceye.py

--- apps/electriceye.py
+++ apps/electriceye.py
@@ -238,7 +238,7 @@ navbar = dbc.Navbar(
                 [
                     dbc.Col(html.Img(src=PLOTLY_LOGO, height="50px"),width=1),
                     dbc.Col(dbc.NavbarBrand("Últimos eventos destacados - Electric Eye"),width=10),
-                    dbc.Col(dbc.Button("Ovdapp", color="primary",outline=True, className="mr-1",id='volver-home',href='http://localhost:8080'),width=1)
+                    dbc.Col(dbc.Button("Ovdapp", color="primary",outline=True, className="mr-1",id='volver-home',href='http://http://172.16.47.13:8080/'),width=1)
                     
                 ],
                 align="left",style={'width':'100%'},
